c046855eef09f41fa3e047e2408aa14b8cda716a
ArthurHelias <arthurhelias37@gmail.com>
2023-01-26 10:44:54 +0100

    Fixed pull.

--- target/classes/data-postgresql.sql
+++ target/classes/data-postgresql.sql
@@ -1,5 +1,7 @@
-INSERT INTO adult (lastName, firstName, image, emotion) VALUES ('Girard', 'Antoine', 'blabla.png', 'sad');
-INSERT INTO adult (lastName, firstName, image, emotion) VALUES ('Mikos', 'Taddeo', 'blabla.png', 'angry');
-INSERT INTO adult (lastName, firstName, image, emotion) VALUES ('Barraud', 'Romain', 'blabla.png', 'love');
-INSERT INTO adult (lastName, firstName, image, emotion) VALUES ('Dupont', 'Pierre', 'blabla.png', 'strong');
-INSERT INTO adult (lastName, firstName, image, emotion) VALUES ('Lebeau', 'Damien', 'blabla.png', 'neutral');
+INSERT INTO family (nameFamily) VALUES ('Girard');
+INSERT INTO family (nameFamily) VALUES ('Mikos');
+INSERT INTO family (nameFamily) VALUES ('Barraud');
+INSERT INTO family (nameFamily) VALUES ('Dupont');
+INSERT INTO family (nameFamily) VALUES ('Joubert');
+INSERT INTO user (firstNameUser, lastNameUser, typeUser, idFamily) values ('Elea', 'Joubert', 'Enfant');
+INSERT INTO user (firstNameUser, lastNameUser, typeUser, idFamily) values ('Jean', 'Joubert', 'Enfant', 5);
--- target/classes/schema-postgresql.sql
+++ target/classes/schema-postgresql.sql
@@ -1,2 +1,20 @@
-create sequence adult_seq;
-create table adult (id bigint default nextval('adult_seq'), firstName varchar(255), lastName varchar(255), image varchar(255), emotion varchar(255), primary key (id));
+create sequence family_seq;
+create table family
+(
+    idFamily   bigint default nextval('family_seq'),
+    nameFamily varchar(255),
+    primary key (idFamily)
+);
+create sequence user_seq;
+create table user
+(
+    idUser        bigint default nextval('user_seq'),
+    firstNameUser varchar(255),
+    lastNameUser  varchar(255),
+    typeUser      varchar(255),
+    idFamily      bigint,
+    primary key (idUser),
+    CONSTRAINT fk_family
+        FOREIGN KEY (idFamily)
+            REFERENCES family (idFamily)
+);
