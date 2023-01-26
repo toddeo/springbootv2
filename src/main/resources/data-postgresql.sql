cdcf043cb8f3a12a5ed7a945e6e685127e4a6d27
ArthurHelias <arthurhelias37@gmail.com>
2023-01-26 10:26:11 +0100

    Changed the queries.

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -2,4 +2,6 @@ INSERT INTO family (nameFamily) VALUES ('Girard');
 INSERT INTO family (nameFamily) VALUES ('Mikos');
 INSERT INTO family (nameFamily) VALUES ('Barraud');
 INSERT INTO family (nameFamily) VALUES ('Dupont');
-INSERT INTO family (nameFamily) VALUES ('Lebeau');
+INSERT INTO family (nameFamily) VALUES ('Joubert');
+INSERT INTO user (firstNameUser, lastNameUser, typeUser, idFamily) values ('Elea', 'Joubert', 'Enfant');
+INSERT INTO user (firstNameUser, lastNameUser, typeUser, idFamily) values ('Jean', 'Joubert', 'Enfant', 5);
--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,2 +1,20 @@
 create sequence family_seq;
-create table family (idFamily bigint default nextval('family_seq'), nameFamily varchar(255),  primary key (idFamily));
+create table family
+(
+    idFamily   bigint default nextval('family_seq'),
+    nameFamily varchar(255),
+    primary key (idFamily)
+);
+create sequence user_seq;
+create table user
+(
+    idUser        bigint default nextval('user_seq'),
+    firstNameUser varchar(255),
+    lastNameUser  varchar(255),
+    typeUser      varchar(255),
+    idFamily      bigint,
+    primary key (idUser),
+    CONSTRAINT fk_family
+        FOREIGN KEY (idFamily)
+            REFERENCES family (idFamily)
+);
