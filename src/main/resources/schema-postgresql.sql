3f4c20d59a5a45bbc9c80573407ecf1dde7deb22
ArthurHelias <arthurhelias37@gmail.com>
2023-01-26 10:14:57 +0100

    Added the User object, refactored the Adult object into the User object.

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,2 +1,2 @@
 create sequence family_seq;
-create table family (id bigint default nextval('family_seq'), nameFamily varchar(255),  primary key (id));
+create table family (idFamily bigint default nextval('family_seq'), nameFamily varchar(255),  primary key (idFamily));
