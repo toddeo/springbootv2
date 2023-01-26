a28f7fc764165c50cd2edbd3fdf8f64f701eda49
ArthurHelias <arthurhelias37@gmail.com>
2023-01-26 11:22:13 +0100

    Added the id in the URL

--- src/main/kotlin/com/example/Mooday/Service/Controller/FamilyController.kt
+++ src/main/kotlin/com/example/Mooday/Service/Controller/FamilyController.kt
@@ -3,6 +3,7 @@ package com.example.mooday.service.controller
 import com.example.mooday.Family
 import com.example.mooday.service.FamilyService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -11,13 +12,13 @@ import java.util.*
 @RequestMapping("api/v1")
 class FamilyController(val familyService: FamilyService) {
 
-    @GetMapping( "/families")
+    @GetMapping("/families")
     fun getAllFamilies(): List<Family> {
         return familyService.getAllFamilies()
     }
 
-    @GetMapping("/family")
-    fun getFamilyById(id: Long): Optional<Family> {
+    @GetMapping("/family/{familyId}")
+    fun getFamilyById(@PathVariable(value = "familyId") id: Long): Optional<Family> {
         return familyService.getFamilyById(id)
     }
 }
--- src/main/kotlin/com/example/Mooday/Service/Controller/UserController.kt
+++ src/main/kotlin/com/example/Mooday/Service/Controller/UserController.kt
@@ -3,6 +3,7 @@ package com.example.mooday.service.controller
 import com.example.mooday.service.UserService
 import com.example.mooday.User
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -11,13 +12,13 @@ import java.util.*
 @RequestMapping("api/v1")
 class UserController(val userService: UserService) {
 
-    @GetMapping( "/users")
+    @GetMapping("/users")
     fun getAllUsers(): List<User> {
         return userService.getAllUsers()
     }
 
-    @GetMapping("/user")
-    fun getUserById(id: Long): Optional<User> {
+    @GetMapping("/user/{userId}")
+    fun getUserById(@PathVariable(value = "userId") id: Long): Optional<User> {
         return userService.getUserById(id)
     }
 }
